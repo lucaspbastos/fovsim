b9723a35ce008fb34c13e853044b1ea0ca91c87a
Lucas Bastos <lucas@lucasbastos.com>
2021-02-07 23:06:48 -0500

    Switched from arc to lines

--- Sensor.js
+++ Sensor.js
@@ -9,33 +9,62 @@ class Sensor {
         this.NAME = NAME;
     }
 
-    drawStation() {
-        fill('rgba(0, 0, 0, 1)');
-        circle(this.X, this.Y, 10);
-        text(this.NAME, this.X-10, this.Y-10);
-        return 1;
-    }
 
     drawSafe(ideal = this.LEN) {
-        fill(color('rgba(0, 255, 0, 0.2)'));
-        arc(this.X, this.Y, ideal*sizescale, ideal*sizescale, 0-this.DEG, this.H_FOV-this.DEG, PIE);
+        let lastX = this.X;
+        let lastY = this.Y;
+        let x2;
+        let y2;
+        for (let i = (this.DEG-this.H_FOV/2); i < (this.DEG+this.H_FOV/2); i+=0.5) {
+            stroke('green');
+            x2 = this.X-cos(180-i)*sizescale*ideal;
+            y2 = this.Y-sin(180-i)*sizescale*ideal;
+            line(this.X, this.Y, x2, y2);
+            line(x2, y2, lastX, lastY);
+            lastX = x2;
+            lastY = y2;
+        }
+
         return 1;
     }
 
     drawUnsafe() {
-        fill(color('rgba(255, 0, 0, 0.5)'));
-        arc(this.X, this.Y, 0.5*sizescale, 0.5*sizescale, 0-this.DEG, this.H_FOV-this.DEG, PIE);
+        stroke(color('rgba(255, 0, 0, 0.5)'));
+        //arc(this.X, this.Y, 0.5*sizescale, 0.5*sizescale, 0-this.DEG, this.H_FOV-this.DEG, PIE);
+
         return 1;
+    }
+
+    drawMidline() {
+        stroke('blue');
+        line(this.X, this.Y, this.X-cos(180-this.DEG)*sizescale, this.Y-sin(180-this.DEG)*sizescale);
+        return 1;
+    }
+
+    drawStation() {
+        fill(color('rgba(0, 0, 0, 1)'));
+        circle(30,30,10)
     }
 
     drawAll() {
         this.drawSafe();
         this.drawUnsafe();
         this.drawStation();
+        this.drawMidline();
+        return 1;
     }
 
     setSync() {
         this.sync = true;
         return 1;
+    }
+
+    setDeg(DEG) {
+        this.deg = DEG;
+        return 1;
+    }
+
+    getDeg() {
+        return this.deg;
     }
 }
--- canvas.js
+++ canvas.js
@@ -1,32 +1,34 @@
 "use strict"    
-const sizescale = 100;
+const sizescale = 70;
 const METER_X = 10;
 const METER_Y = 10;
 const INDEX = true;
+let slider;
 
 function setup() {
-    createCanvas(METER_X*sizescale, METER_Y*sizescale);
+    createCanvas(METER_X*sizescale+60, METER_Y*sizescale+60);
     angleMode(DEGREES);
+    slider = createSlider(0,360,0,1);
+
 }
 
+let load = false;
+let lastdeg = 0;
 function draw() {
     background(220);
     fill(color(255,255,255));
 
-    // main: x 124 in, y 137 in
     const mainx = inchesToMeters(124);
     const mainy = inchesToMeters(137);
     room(mainx*sizescale, mainy*sizescale);
-
-    //office: x 98 in, y 56 in
+    //room(7*sizescale, 7*sizescale);
     const officex = inchesToMeters(98);
     const officey = inchesToMeters(56);
     room((mainx+officex)*sizescale, officey*sizescale);
 
-    let b1 = new BaseStation2(50, 50, 30);
+    let deg = slider.value();
+    let b1 = new BaseStation2(30, 30, deg);
     b1.drawAll();
-
-    noLoop();
 }
 
 function inchesToMeters(inches) {
